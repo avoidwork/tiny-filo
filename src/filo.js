0c139c970b9e5111f60efa3c56adb728b1db3bb2
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-01-14 08:58:54 -0500

    Refactoring `order` to pointer properties to remove iteration cost of `order.shift()` on eviction or deletion

--- src/filo.js
+++ src/filo.js
@@ -1,7 +1,8 @@
 	class FILO {
 		constructor (max = 0, ttl = 0) {
+			this.first = null;
 			this.items = {};
-			this.order = [];
+			this.last = null;
 			this.max = max;
 			this.size = 0;
 			this.ttl = ttl;
@@ -12,25 +13,45 @@
 		}
 
 		clear () {
+			this.first = null;
 			this.items = {};
-			this.order.length = 0;
+			this.last = null;
 			this.size = 0;
 		}
 
-		delete (key, idx = -1) {
+		delete (key) {
 			if (this.has(key)) {
+				const item = this.items[key];
+
 				delete this.items[key];
-				this.order.splice(idx > -1 ? idx : this.order.indexOf(key), 1);
 				this.size--;
+
+				if (item.prev !== null) {
+					item.prev.next = item.next;
+				}
+
+				if (item.next !== null) {
+					item.next.prev = item.prev;
+				}
+
+				if (this.first === item) {
+					this.first = item.next;
+				}
+
+				if (this.last === item) {
+					this.last = item.prev;
+				}
 			}
 
 			return this;
 		}
 
 		evict () {
-			const key = this.order.pop();
+			const item = this.last;
 
-			delete this.items[key];
+			delete this.items[item.key];
+			this.last = item.prev;
+			this.last.next = null;
 			this.size--;
 		}
 
@@ -51,7 +72,7 @@
 		}
 
 		keys () {
-			return this.order;
+			return Object.keys(this.items);
 		}
 
 		set (key, value) {
@@ -64,9 +85,21 @@
 					this.evict();
 				}
 
-				this.order.push(key);
-				this.items[key] = {expiry: this.ttl > 0 ? new Date().getTime() + this.ttl : this.ttl, value};
-				this.size++;
+				const item = this.items[key] = {
+					expiry: this.ttl > 0 ? new Date().getTime() + this.ttl : this.ttl,
+					key: key,
+					prev: this.last,
+					next: null,
+					value
+				};
+
+				if (++this.size === 1) {
+					this.first = item;
+					this.last = item;
+				} else {
+					this.last.next = item;
+					this.last = item;
+				}
 			}
 
 			return this;
--- test/filo.js
+++ test/filo.js
@@ -3,7 +3,7 @@
 const path = require("path"),
 	filo = require(path.join("..", "lib", "tiny-filo.js"));
 
-exports.simple = {
+exports.evict = {
 	setUp: function (done) {
 		this.cache = filo(4);
 		this.items = ["a", "b", "c", "d", "e"];
@@ -11,12 +11,38 @@ exports.simple = {
 	},
 	test: function (test) {
 		this.items.forEach(i => this.cache.set(i, false));
-		test.expect(4);
-		test.equal(JSON.stringify(this.cache.order), JSON.stringify(["a", "b", "c", "e"]), `Should be '${JSON.stringify(["a", "b", "c", "e"])}'`);
+		test.expect(6);
+		test.equal(this.cache.first.key, "a", "Should be 'a'");
+		test.equal(this.cache.last.key, "e", "Should be 'e'");
 		test.equal(this.cache.size, 4, "Should be '4'");
 		this.cache.evict();
-		test.equal(JSON.stringify(this.cache.order), JSON.stringify(["a", "b", "c"]), `Should be '${JSON.stringify(["a", "b", "c"])}'`);
+		test.equal(this.cache.first.key, "a", "Should be 'a'");
+		test.equal(this.cache.last.key, "c", "Should be 'c'");
 		test.equal(this.cache.size, 3, "Should be '3'");
+		test.done();
+	}
+};
+
+exports.deletion = {
+	setUp: function (done) {
+		this.cache = filo(4);
+		this.items = ["a", "b", "c", "d", "e"];
+		done();
+	},
+	test: function (test) {
+		this.items.forEach(i => this.cache.set(i, false));
+		test.expect(9);
+		test.equal(this.cache.first.key, "a", "Should be 'a'");
+		test.equal(this.cache.last.key, "e", "Should be 'e'");
+		test.equal(this.cache.size, 4, "Should be '4'");
+		this.cache.delete("b");
+		test.equal(this.cache.first.key, "a", "Should be 'a'");
+		test.equal(this.cache.last.key, "e", "Should be 'e'");
+		test.equal(this.cache.size, 3, "Should be '3'");
+		this.cache.delete("e");
+		test.equal(this.cache.first.key, "a", "Should be 'a'");
+		test.equal(this.cache.last.key, "c", "Should be 'c'");
+		test.equal(this.cache.size, 2, "Should be '2'");
 		test.done();
 	}
 };
