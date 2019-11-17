3957a5d5c0b5b44a27ca7d3ad2103c2d0f0f0622
Jason Mulligan <jason.mulligan@avoidwork.com>
2019-11-17 10:59:14 -0500

    Reverting 1.1.0 change to a `Map`, adding benchmark which is exposing a significant deopt on high key eviction

---
+++ benchmark.js
@@ -0,0 +1,68 @@
+const path = require("path"),
+	fifo = require(path.join(__dirname, "lib", "tiny-filo.js")),
+	precise = require("precise"),
+	nth = 2e4,
+	cache = fifo(nth),
+	data = new Array(nth);
+
+function seed () {
+	let i = -1;
+
+	while (++i < nth) {
+		data[i] = Math.floor(Math.random() * nth);
+	}
+}
+
+function populate (arg, start = 0) {
+	const nth = arg.max;
+	let i = -1;
+
+	while (++i < nth) {
+		arg.set(i + start, data[i]);
+	}
+}
+
+function get (arg, start = 0) {
+	const nth = arg.max;
+	let i = -1;
+
+	while (++i < nth) {
+		arg.get(i + start, data[i]);
+	}
+}
+
+function bench (n = 0, x = 1, type = "set") {
+	if (type === "set") {
+		seed();
+
+		const timer = precise().start();
+
+		try {
+			populate(cache, n);
+		} catch (err) {
+			console.error(err.status);
+		}
+
+		timer.stop();
+
+		const f = timer.diff() / 1e6;
+
+		console.log(`Run ${x} ${x === 1 ? "Set" : "Evict"} (${n === 0 ? "Low Keys" : "High Keys"}): ${f} ms (${Math.ceil(nth / f)} ops/ms)`);
+	} else if (type === "get") {
+		const timer = precise().start();
+
+		get(cache, n);
+		timer.stop();
+
+		const f = timer.diff() / 1e6;
+
+		console.log(`Run ${x} Get (${n === 0 ? "Low Keys" : "High Keys"}): ${f} ms (${Math.ceil(nth / f)} ops/ms)`);
+	}
+}
+
+console.log(`Benchmarking ${nth} items (random value per run)`);
+bench();
+bench(nth, 2);
+bench(void 0, 3);
+bench(nth, 4);
+bench(nth, 5, "get");
